2541b22aa7b1f0377c99861737b7a177c639aa75
JWest2323 <jwest23@ufl.edu>
2022-05-18 14:11:38 -0500

    home component

--- video-game-db/src/app/app-routing.module.ts
+++ video-game-db/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
+import { HomeComponent } from './components/home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'search/:game-search',
+    component: HomeComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- video-game-db/src/app/app.module.ts
+++ video-game-db/src/app/app.module.ts
@@ -13,11 +13,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SearchBarComponent } from './components/search-bar/search-bar.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SearchBarComponent
+    SearchBarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
