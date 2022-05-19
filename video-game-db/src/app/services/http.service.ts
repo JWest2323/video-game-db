952b39dc554544e8d3d33a219ccb3be7a314f91a
JWest2323 <jwest23@ufl.edu>
2022-05-18 23:55:35 -0500

    http service & interfaces

---
+++ video-game-db/src/app/services/http.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment as env } from 'src/environments/environment';
+import { APIResponse, Game } from 'src/app/models';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpService {
+
+  constructor(private http: HttpClient) { }
+
+  getGameList(
+    ordering: string,
+    search?: string
+  ): Observable<APIResponse<Game>> {
+    let params = new HttpParams().set('ordering', ordering);
+
+    if (search) {
+      params = new HttpParams().set('ordering', ordering).set('search', search);
+    }
+
+    return this.http.get<APIResponse<Game>>(`${env.BASE_URL}/games`, {
+      params: params,
+    });
+
+  }
+}
