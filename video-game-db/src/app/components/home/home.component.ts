d624ec64929a1c93c9cd75e0c6006bf4ca046859
JWest2323 <jwest23@ufl.edu>
2022-05-27 00:46:14 -0500

    home component: searchGames func & ngContainer logic

--- video-game-db/src/app/components/home/home.component.ts
+++ video-game-db/src/app/components/home/home.component.ts
@@ -1,4 +1,8 @@
+import { APIResponse } from 'src/app/models';
+import { HttpService } from './../../services/http.service';
 import { Component, OnInit } from '@angular/core';
+import { Game } from '../../models';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +11,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   public sort = '';
-  constructor() { }
+  public games!: Array<Game>;
+  constructor(
+    private httpService: HttpService,
+    private activatedRoute: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    this.activatedRoute.params.subscribe((params: Params) => {
+      if (params['game-search']) {
+        this.searchGames('metacrit', params['game-search']);
+      } else {
+        this.searchGames('metacrit');
+      }
+    });
+  }
+
+  searchGames(sort: string, search?: string): void {
+    this.httpService.getGameList(sort, search).subscribe((gameList: APIResponse<Game>) => {
+      this.games = gameList.results;
+      console.log(gameList);
+    });
   }
 
 }
