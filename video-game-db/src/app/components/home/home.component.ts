62165c9acabd856344768d1743c9137dea922a08
JWest2323 <jwest23@ufl.edu>
2022-05-31 00:05:35 -0500

    sort menu implementation

--- video-game-db/src/app/components/home/home.component.ts
+++ video-game-db/src/app/components/home/home.component.ts
@@ -1,24 +1,29 @@
 import { APIResponse } from 'src/app/models';
 import { HttpService } from './../../services/http.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Game } from '../../models';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
   public sort = '';
   public games!: Array<Game>;
+  private routeSub?: Subscription;
+  private gameSub?: Subscription;
+
   constructor(
     private httpService: HttpService,
+    private router: Router,
     private activatedRoute: ActivatedRoute
   ) { }
 
   ngOnInit(): void {
-    this.activatedRoute.params.subscribe((params: Params) => {
+    this.routeSub = this.activatedRoute.params.subscribe((params: Params) => {
       if (params['game-search']) {
         this.searchGames('metacrit', params['game-search']);
       } else {
@@ -28,10 +33,23 @@ export class HomeComponent implements OnInit {
   }
 
   searchGames(sort: string, search?: string): void {
-    this.httpService.getGameList(sort, search).subscribe((gameList: APIResponse<Game>) => {
+    this.gameSub = this.httpService.getGameList(sort, search).subscribe((gameList: APIResponse<Game>) => {
       this.games = gameList.results;
       console.log(gameList);
     });
+  }
+
+  openGameDetails(id: string): void {
+    this.router.navigate(['details', id]);
+  }
+
+  ngOnDestroy(): void {
+    if (this.gameSub) {
+      this.gameSub.unsubscribe();
+    }
+    if (this.routeSub) {
+      this.routeSub.unsubscribe();
+    }
   }
 
 }
--- video-game-db/src/app/models.ts
+++ video-game-db/src/app/models.ts
@@ -1,6 +1,7 @@
 export interface Game {
     background_image: string;
     name: string;
+    id: string;
     released: string;
     metacritic_url: string;
     website: string;
